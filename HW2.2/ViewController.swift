07dd301ebb159f5a855170f7144cbcd7e6e2da22
Alex Tishchenko <a.tishencko2017@yandex.ru>
2020-01-31 18:41:05 +0300

    1

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    @IBOutlet var redLabelText: UILabel!
+    @IBOutlet var greenLabelText: UILabel!
+    @IBOutlet var blueLabelText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 255
+        redSlider.value = 0
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 255
+        blueSlider.value = 0
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 255
+        greenSlider.value = 0
+        
+        redLabelText.text = String(Int(redSlider.value))
+        greenLabelText.text = String(format: "%.2f", greenSlider.value)
+        blueLabelText.text = String(format: "%.2f", blueSlider.value)
+        
+        
+        
     }
-
-
+    
+    @IBAction func redActionSlider() {
+        
+    }
+    
+    
+    @IBAction func greenActionSlider() {
+        
+    }
+    
+    @IBAction func blueActionSlider() {
+        
+    }
+    
+    func changeColorView() {
+        let changeColor = UIColor(red: redSlider?.value,
+                                  green: greenSlider.value,
+                                  blue: blueSlider.value, alpha: 1)
+    }
 }
-
